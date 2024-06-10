93a7b802147e146742875b2c272d2498ce13c4ca
RifatMahmudno-1 <rifatmahmudpc@gmail.com>
2024-06-10 15:39:10 +0600

    typo

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,11 +3,11 @@ import IconHeart from './IconHeart'
 function Comp() {
 	return (
 		<footer className="bg-cyan-400 flex items-center gap-1 justify-center p-1">
-			Made with{' '}
+			Made with
 			<span className="text-rose-500 text-xl">
 				<IconHeart />
-			</span>{' '}
-			by{' '}
+			</span>
+			by
 			<a className="font-semibold" target="_blank" href="https://github.com/RifatMahmudno-1">
 				Rifat Mahmud
 			</a>
